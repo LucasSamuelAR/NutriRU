1bb7c8ae2c8948654b14b6abb0c4e75df8fb1213
LucasSamuelAR <8699497028t@gmail.com>
2026-05-26 12:54:57 -0300

    NutriRU

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/inicio" element={<Inicio />} />
         <Route path="/intencao" element={<Intencao />} />
